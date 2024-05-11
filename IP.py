235f40a49d77c471528eba71955c652b050d813d
git03-Nguyen <nguyendinhanhvlqt@gmail.com>
2024-05-12 04:10:29 +0700

    update comments

--- IP.py
+++ IP.py
@@ -2,31 +2,34 @@ import numpy as np
 from scipy.optimize import LinearConstraint
 from scipy.optimize import milp
 
-# Nhập hệ số của hàm mục tiêu, dạng tìm GTNN:
+# Nhap he so ham muc tieu, dang tim min
 c = np.array([230, 250, 280, 250, 300])
 
-# Nhập hệ số của các ràng buộc (đã được chuẩn hoá) vào ma trận
+# Nhap he so cua cac rang buoc
 A = np.array([[1, 0, 0, 1, 0],
               [1, 1, 0, 0, 1],
               [0, 1, 1, 1, 0],
               [0, 0, 1, 0, 1]])
 
-# Chặn dưới của các ràng buộc:
+# Chan duoi cua cac rang buoc
 b_l = np.array([9, 20, 11, 17])
-
-# Chặn trên của các ràng buộc: không có chặn trên
+# Chan tren cua cac rang buoc (khong co, vi tat ca deu la >=)
 b_u = np.inf * np.ones_like(b_l)
 
-# Khởi tạo đối tượng hệ ràng buộc scipy.optimize.LinearConstraint:
+# Khoi tao doi tuong he rang buoc scipy.optimize.LinearConstraint:
 constraints = LinearConstraint(A, b_l, b_u)
 
-# Mặc định, các biến x_i sẽ được hiểu là không âm nên ta không cần cung cấp thêm thông tin đó.
-# Ngoài ra, tất cả các biến x_i  đều là số nguyên:
+# Mac dinh, cac bien x_i se duoc hieu la khong am nen ta khong can cung cap them thong tin do.
+# Ngoai ra, tat ca cac bien x_i deu la so nguyen:
 integrality = np.ones_like(c)
 
-# Kết quả bài toán:
+# Ket qua bai toan:
 res = milp(c=c, constraints=constraints, integrality=integrality)
 print(res.x)
+
+
+
+
 
 
 
